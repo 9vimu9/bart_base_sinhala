2f182d88e6c3a659e4c5ecf7a0bd3cc6b2783c4a
wimukthi <9wimu9@gmail.com>
2023-05-03 08:54:31 +0530

    remove unwanted code

--- run_bart_dlm_flax.py
+++ run_bart_dlm_flax.py
@@ -40,7 +40,7 @@ import jax.numpy as jnp
 import nltk
 import numpy as np
 import optax
-from datasets import load_dataset
+from datasets import load_dataset, load_from_disk
 from flax import jax_utils, traverse_util
 from flax.jax_utils import pad_shard_unpad
 from flax.training import train_state
@@ -618,68 +618,8 @@ def main():
 
     max_seq_length = min(data_args.max_seq_length, tokenizer.model_max_length)
 
-    # Use Punkt Sentence Tokenizer to divide a document into a list of sentences
-    nltk.download("punkt")
-    sentence_tokenizer = nltk.data.load("tokenizers/punkt/english.pickle")
-
-    def sentence_split_function(example):
-        sents = sentence_tokenizer.tokenize(example["text"])
-        # use pad token as end of sentence indicator
-        new_text = tokenizer.bos_token + f"{tokenizer.pad_token}".join(sents) + tokenizer.eos_token
-        return {"text": new_text}
-
-    split_datasets = datasets.map(
-        sentence_split_function,
-        batched=False,
-        num_proc=data_args.preprocessing_num_workers,
-        remove_columns=column_names,
-        load_from_cache_file=not data_args.overwrite_cache,
-    )
-
-    # Tokenize every text, then concatenate them together before splitting them in smaller parts.
-    # Since we make sure that all sequences are of the same length, no attention_mask is needed.
-    def tokenize_function(examples):
-        return tokenizer(examples[text_column_name], add_special_tokens=False, return_attention_mask=False)
-
-    tokenized_datasets = split_datasets.map(
-        tokenize_function,
-        batched=True,
-        num_proc=data_args.preprocessing_num_workers,
-        remove_columns=text_column_name,
-        load_from_cache_file=not data_args.overwrite_cache,
-    )
-
-    # Main data processing function that will concatenate all texts from our dataset and generate chunks of
-    # max_seq_length.
-    def group_texts(examples):
-        # Concatenate all texts.
-        concatenated_examples = {k: list(chain(*examples[k])) for k in examples.keys()}
-        total_length = len(concatenated_examples[list(examples.keys())[0]])
-        # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
-        # customize this part to your needs.
-        if total_length >= max_seq_length:
-            total_length = (total_length // max_seq_length) * max_seq_length
-        # Split by chunks of max_len.
-        result = {
-            k: [t[i : i + max_seq_length] for i in range(0, total_length, max_seq_length)]
-            for k, t in concatenated_examples.items()
-        }
-        return result
-
-    # Note that with `batched=True`, this map processes 1,000 texts together, so group_texts throws away a
-    # remainder for each of those groups of 1,000 texts. You can adjust that batch_size here but a higher value
-    # might be slower to preprocess.
-    #
-    # To speed up this part, we use multiprocessing. See the documentation of the map method for more information:
-    # https://huggingface.co/docs/datasets/package_reference/main_classes.html#datasets.Dataset.map
-    tokenized_datasets = tokenized_datasets.map(
-        group_texts,
-        batched=True,
-        num_proc=data_args.preprocessing_num_workers,
-        load_from_cache_file=not data_args.overwrite_cache,
-    )
     # load tokenized_datasets from disk
-    tokenized_datasets = tokenized_datasets.load_from_disk(TOKENIZED_DATASET_DIR_PATH)
+    tokenized_datasets = load_from_disk(TOKENIZED_DATASET_DIR_PATH)
     # Enable tensorboard only on the master node
     has_tensorboard = is_tensorboard_available()
     if has_tensorboard and jax.process_index() == 0:
